27edf2c049237cfa5bdd6cce190a4994996efcc1
kobe63 <kobe.reygel@hotmail.com>
2018-03-08 18:14:19 +0100

    ParseHTML

--- src/ParseHTML.java
+++ src/ParseHTML.java
@@ -4,9 +4,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
-public class Parser {
+public class ParseHTML {
     // A class using the JSoup library, to parse the incoming HTML file and retrieve embedded objects.
-
+    public void ParseHTML(){
+    }
     public Elements scanForEmbeddedObjects(String htmlstring){
         Document doc = Jsoup.parse(htmlstring);
         Elements images = doc.select("img");
