6728d3067084bb610f9818a1f2888cf53f04b90e
kobe63 <kobe.reygel@hotmail.com>
2018-03-14 15:10:51 +0100

    htapheadrequest

--- src_client/HttpHeadRequest.java
+++ src_client/HttpHeadRequest.java
@@ -14,6 +14,58 @@ public class HttpHeadRequest extends HttpRequest {
 
     @Override
     public String execute(PrintWriter outputWriter, DataInputStream inputReader) {
-        return "";
+        URL url = this.getUrl();
+        String host = url.getHost();
+        String path = url.getPath();
+        String requestMessage[] = buildHeadRequest(host, path);
+
+        //send the newly created message
+        sendRequestHeader(requestMessage, outputWriter);
+
+        //read the input from the stream
+        return receiveResponse(inputStream);
+
+    }
+
+
+    private String receiveResponse(DataInputStream inputStream){
+        //initialize the strings
+
+        String response = null;
+        //get the input stream reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = null;
+        //initialize the builder
+        StringBuilder responseBodyBuilder = new StringBuilder(reader);
+
+        while((line = reader.readLine)!=null) {
+            //append the line
+            responseBodyBuilder.append(line);
+            //also add newline feed
+            responseBodyBuilder.append("\n");
+            }
+
+
+        response = responseBodyBuilder.toString();
+        this.saveHtmlPage(response, "HeadResult");
+
+        return ;
+    }
+
+
+    private static String[] buildHeadRequest(String host, String path){
+        if(path.equals("")){
+            path = "/";
+        }
+        //generate he string array
+        String request[] = new String[NB_LINES_IN_COMMAND];
+        //generate the first line
+        request[0] = GET + " " + path + " " + HTTP_VERSION;
+        //also add the host
+        request[1] = HOST + host;
+        //request to keep the connection alive
+        request[2] = KEEP_CONNECTION_ALIVE;
+
+        return request;
     }
 }
